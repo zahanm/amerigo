5ad7e2c7316fb5925e754ac5946966708d6c777b
Zahan Malkani <zahanm@gmail.com>
2013-01-18 18:09:42 -0800

    branchCheck working

--- vespucci.js
+++ vespucci.js
@@ -1,14 +1,50 @@
 
 'use strict';
 
+var fs = require('fs');
 var spawn = require('child_process').spawn;
 
 "This directory ain't ready to go on a journey just yet."
 "Missing 'journey.json'"
 
-function voyage() {
+var COMMAND = 'rsync';
+var CONFIG_FILE = 'journey.json';
+var SSH = 'ssh';
+var GITB = 'git branch'
+
+function branchCheck(config, cb) {
+  var check = "cd " + config.root + " && " + GITB;
+  var args = [ config.host, check ];
+  var ssh = spawn(SSH, args);
+  var output = '';
+  ssh.stdout.on('data', function onOut(data) {
+    output += String(data);
+  });
+  ssh.stderr.on('data', function onOut(data) {
+    output += String(data);
+  });
+  ssh.on('exit', function(code) {
+    if (code !== 0) abortJourney("Try ssh'ing into " + config.host);
+    var remotebranch = output.split('\n').filter(function(line) {
+      return !!/^\*/.exec(line);
+    });
+    if (remotebranch.length !== 1)
+      abortJourney('Check your git dir on ' + config.host + ':' + config.root);
+    remotebranch = remotebranch[0].slice(2);
+    var localbranch = ARGV._[1];
+    if (localbranch != remotebranch)
+      abortJourney('Branch mismatch => remote:' + remotebranch + ' != local:' + localbranch);
+    cb(config);
+  });
+}
+
+function voyage(config) {
   var downloading = ARGV._[0] == 'down';
-  rsync --verbose --rsh=ssh --compress --recursive --dry-run
+  "rsync --verbose --rsh=ssh --compress --recursive --dry-run"
+  var args = [ '--rsh=ssh', '--compress', '--recursive' ];
+  if (ARGV.verbose) args.push('--verbose');
+  if (ARGV['dry-run']) args.push('--dry-run');
+  console.log("success");
 }
 
 function prepareVessel() {
@@ -21,7 +57,18 @@ function prepareVessel() {
     return passing && val;
   }, true);
   if (!goodArgs) abortJourney('Arguments incorrect');
-  voyage();
+  if (!fs.existsSync(CONFIG_FILE) || !fs.statSync(CONFIG_FILE).isFile())
+    abortJourney(CONFIG_FILE + ' missing');
+  fs.readFile(CONFIG_FILE, function onRead(err, contents) {
+    if (err) abortJourney("File reading error: " + CONFIG_FILE);
+    var config = {};
+    try {
+      config = JSON.parse(contents);
+    } catch (err) {
+      abortJourney("JSON parsing error: " + CONFIG_FILE);
+    }
+    branchCheck(config, voyage);
+  });
 }
 
 function abortJourney(err) {
